9f56eaba71e7fbb65dd497203dcea69b0c21f767
Rozbur <rozbur@mail.ru>
2019-11-25 18:09:54 +0500

    Написан код для морского мобя из 1 корабля. и убрал пробел пробел

--- src/SeaBattle/GameLauncher.java
+++ src/SeaBattle/GameLauncher.java
@@ -2,7 +2,7 @@ package SeaBattle;
 
 public class GameLauncher {
     public static void main(String[] args){
-        CheckShot dot=new CheckShot();                  //Создаю экземпляр класса CheckShot
+        CheckShot dot=new CheckShot();              //Создаю экземпляр класса CheckShot
         int[] locations={2,3,4};                    //Создаю массив для местоположения коробля
 
         dot.setLocationShip(locations);             //Вызываю сеттер CheckShot'а. т.е отправляю координаты корабля из массива locations в объект CheckShot(там их принимает тоже массив)
